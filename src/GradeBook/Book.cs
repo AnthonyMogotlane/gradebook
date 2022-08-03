9fb799dc782562b2bfb0dc05a81fd2311a75bee0
Anthony Mogotlane <almogotlane@gmail.com>
2022-08-03 23:28:23 +0200

    Added ShowStatistics method

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -7,6 +7,26 @@ public class Book
     public Book() => grades = new List<double>();
     public void AddGrade(double grade)
     {
-        Console.WriteLine(grade);
+        grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+        double res = 0.0;
+        double highGrade = double.MinValue;
+        double lowestGrade = double.MaxValue;
+
+        foreach(double num in grades)
+        {
+            lowestGrade = Math.Min(num, lowestGrade);
+            highGrade = Math.Max(num, highGrade);
+            res += num;
+        }
+
+        double avg = res/grades.Count;
+
+        Console.WriteLine($"The lowest grade is {lowestGrade}");
+        Console.WriteLine($"The highest grade is {highGrade}");
+        Console.WriteLine($"The lowest grade is {avg:N1}");
     }
 }
