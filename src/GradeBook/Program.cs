fee65677fb0380f7a77eaf677e2db6601a8b7fdf
Anthony Mogotlane <almogotlane@gmail.com>
2022-08-03 23:29:40 +0200

    Modified Program.cs

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,22 +7,10 @@ internal class Program
 
         Book book = new Book();
         book.AddGrade(8.9);
+        book.AddGrade(12.9);
+        book.AddGrade(7.5);
+        book.AddGrade(5);
 
-        // List<double> grades = new List<double>() {12.5, 39.03, 87.74, 88.83};
-        // double sum = 0;
-        // foreach(double grade in grades)
-        // {
-        //     sum += grade;
-        // }
-        // sum /= grades.Count;
-        // Console.WriteLine($"The average is {sum:N1}");
-
-        string hexString = "10";
-        string[] hexArray = hexString.Split("");
-        foreach(var s in hexArray)
-        {
-          Console.WriteLine(s);
-        }
-         Console.WriteLine(hexArray.GetType());
+        book.ShowStatistics();
     }
 }
